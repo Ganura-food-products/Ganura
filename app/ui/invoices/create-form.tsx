be853bb8075b220afd28d54ff24697a012d30dea
ntwalibrian <ntwalibriand@gmail.com>
2025-01-14 09:32:51 +0200

    fix home page

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -84,7 +84,7 @@ export default function Form({ farmers }: { farmers: FarmerField[] }) {
                 name="amount"
                 type="number"
                 step="0.01"
-                placeholder="Enter USD amount"
+                placeholder="Enter RFW amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
                 aria-describedby="amount-error"
               />
