d65e4ea1f410d474ac0d5dc47e4f6943b0071790
ntwalibrian <ntwalibriand@gmail.com>
2025-01-29 12:03:30 +0200

    fix mobile sidenav

--- app/dashboard/unauthorized/page.tsx
+++ app/dashboard/unauthorized/page.tsx
@@ -11,7 +11,7 @@ export default async function Page() {
     <main className="flex h-full flex-col items-center justify-center gap-2">
       <FaceFrownIcon className="w-10 text-gray-400" />
       <h2 className="text-xl font-semibold">404 Unauthorized</h2>
-      <p>you're not allowed to view that page</p>
+      <p>you are not allowed to view that page</p>
       <Link
         href="/dashboard"
         className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
