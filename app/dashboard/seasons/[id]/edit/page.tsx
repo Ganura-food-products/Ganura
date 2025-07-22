89df9550321f15093476064b0a43b402a82b9ae8
Clement Gakire <clementgakire@gmail.com>
2025-07-22 17:27:10 +0200

    typescript errors fix

--- app/dashboard/seasons/[id]/edit/page.tsx
+++ app/dashboard/seasons/[id]/edit/page.tsx
@@ -8,7 +8,8 @@ export const metadata: Metadata = {
   title: 'Edit Season',
 };
 
-export default async function Page({ params }: { params: { id: string } }) {
+export default async function Page(props: { params: Promise<{ id: string }> }) {
+  const params = await props.params;
   const id = params.id;
   const [season] = await Promise.all([fetchSeasonById(id)]);
 
